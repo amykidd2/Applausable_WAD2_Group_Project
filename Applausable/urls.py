a9834ee47898d0f8a512a68a926d5a8bea0cc00d
amykidd2 <59999426+amykidd2@users.noreply.github.com>
2020-02-27 21:32:19 +0000

    created a basic home page and template

--- Applausable/urls.py
+++ Applausable/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rango import views
+
+app_name = 'applausable'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
 ]
