75ddfd8868854832dacacf2bda3959aeaae14982
amykidd2 <59999426+amykidd2@users.noreply.github.com>
2020-02-28 15:05:14 +0000

    database populating

--- populate_rango.py
+++ populate_rango.py
@@ -8,23 +8,48 @@ from rango.models import User, Artist, Song, Review, Album
 def populate():
     arts = [{
         'id': 1000, 'name': 'The Beatles', 'genre': 'Rock', 
-        'albums': {'Abbey Road': {'ablums': abbey_road}}, 
+        'albums': {'albums': beatles_albs}, 
         'description': 'John, Paul, George and Ringo', 
         'linkToSocialMedia': 'https://www.youtube.com/user/thebeatles'
         }]
 
-    abbey_road = [{
-        'id': 1010, 'songs': {'Here Comes The Sun': {'songs': here_comes_the_sun}},
-        'albumName': 'Abbey Road'
+    beatles_albs = [
+        {'id': 1010, 'songs': {'Here Comes The Sun': {'songs': here_comes_the_sun}},
+        'albumName': 'Abbey Road'}
+        {'id': 1020, 'songs': {'Let it be': {'songs': let_it_be}},
+        'albumName': 'Let It Be'}
         #TODO: artistID and just now image is set as null
-    }]
+    ]
 
-    here_comes_the_sun = [{
+    abbey_road_songs = [{
         'id': 1011, 'title' = 'Here Comes The Sun', 'album': 'Abbey Road', 
-        'artist': 'The Beatles', 'genre': 'Rock', 'linkToSong': 'https://www.youtube.com/watch?v=bgiQD56eWDk'
+        'artist': 'The Beatles', 'genre': 'Rock', 
+        'linkToSong': 'https://www.youtube.com/watch?v=bgiQD56eWDk'
         #TODO: artistID and reviews. OverallScore set to 0
 
     }]
+
+
+    here_comes_the_sun = [{
+        'id': 1011, 'title' = 'Here Comes The Sun', 'album': 'Abbey Road', 
+        'artist': 'The Beatles', 'genre': 'Rock', 
+        'linkToSong': 'https://www.youtube.com/watch?v=bgiQD56eWDk'
+        #TODO: artistID and reviews. OverallScore set to 0
+
+    }]
+
+    abbey_road = [
+        {'id': 1010, 'songs': {'Here Comes The Sun': {'songs': here_comes_the_sun}},
+        'albumName': 'Abbey Road'}
+        ]
+
+    the_beatles = [{
+        'id': 1000, 'name': 'The Beatles', 'genre': 'Rock', 
+        'albums': {'albums': abbey_road}, 
+        'description': 'John, Paul, George and Ringo', 
+        'linkToSocialMedia': 'https://www.youtube.com/user/thebeatles'
+        }]    
+
 
     users = [
         {'userID': 54321, 'password': 12345,'professional': False},
@@ -32,6 +57,8 @@ def populate():
         #TODO: add image
         ]
 
+def add_artist(id, name, genre, albums, description, linkToSocialMedia):
+    a = Page.objects.get_or_create(id=id, albums=albums)[0]
 
 def add_user(userID, password, professional):
     u = User.objects.get_or_create(userID=userID)[0]
