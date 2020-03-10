b4b500bc9cfa6c8a75c25efe48fb38c0391b29c1
Euan Wills <2389677W@student.gla.ac.uk>
2020-03-10 14:13:25 +0000

    added song table

--- populate_script.py
+++ populate_script.py
@@ -3,19 +3,34 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Applausable.settings')
  
 import django 
 django.setup() 
-from rango.models import Artist, Album
+from rango.models import Artist, Album, Song
 
 def populate():
 
     #TODO: add songs the albs list and update all of the populate
+
+    abbeyRoad_songs = [
+        {'songID': 1011, 'title': 'Come Together', 'albumID':1010,'artistName':'The Beatles', 'artistID': 1000, 'linkToSong':'https://www.youtube.com/watch?v=45cYwDMibGo'}
+        ]
+    letItBe_songs = [
+        {'songID': 1021, 'title': 'Let It Be', 'albumID':1020,'artistName':'The Beatles', 'artistID': 1000, 'linkToSong':'https://www.youtube.com/watch?v=QDYfEBY9NM4'}
+        ]
+    wtsmg_songs = [
+        {'songID': 2011, 'title': 'Wonderwall', 'albumID':2010,'artistName':'Oasis', 'artistID': 2000, 'linkToSong':'https://www.youtube.com/watch?v=6hzrDeceEKc'}
+        ]
+    definetlyMaybe_songs = [
+        {'songID': 2021, 'title': 'Live Forever', 'albumID':2020,'artistName':'Oasis', 'artistID': 2000, 'linkToSong':'https://www.youtube.com/watch?v=i_2mWhfOhGU'}
+        ]
+
+
     beatles_albs =[
-        {'albumID': 1010, 'albumName': 'Abbey Road'},
-        {'albumID': 1020, 'albumName': 'Let It Be'}
+        {'albumID': 1010, 'albumName': 'Abbey Road','songs':abbeyRoad_songs},
+        {'albumID': 1020, 'albumName': 'Let It Be', 'songs':letItBe_songs}
     ]
      
     oasis_albs = [
-        {'albumID': 2010, 'albumName': '(Whats the Story) Morning Glory?'},
-        {'albumID': 2020, 'albumName': 'Definetly Maybe'}
+        {'albumID': 2010, 'albumName': '(Whats the Story) Morning Glory?', 'artistID':2000,'songs':wtsmg_songs},
+        {'albumID': 2020, 'albumName': 'Definetly Maybe','artistID':2000,'songs':definetlyMaybe_songs}
     ]
      
     arts = {
@@ -25,14 +40,22 @@ def populate():
      
     for arts, arts_data in arts.items():
         a = add_artist(arts, arts_data['artistName'], arts_data['genre'], arts_data['description'], arts_data['LinkToSocialMedia'])
-        for alb in arts_data['albs']:
-            add_album(a, alb['albumID'], alb['albumName'])
-
+        for album in arts_data['albs']:
+            alb = add_album(a, album['albumID'], album['albumName'])
+            for song in album['songs']:
+                add_song(song['songID'], song['title'], alb, a, song['linkToSong'], song['artistName'])
     #Doesn't seem to want to print it out in command line but doesn't effect the actual population        
     #for a in Artist.objects.all():
         #for alb in Album.objects.filter(artistID=a):
             #print(f'- {a}: {alb}')
 
+def add_song(songID, title, alb, a, linkToSong, artistName, overallscore=0):
+    song = Song.objects.get_or_create(songID=songID,albumID=alb,artistID=a)[0]
+    song.title=title
+    song.overallScore=overallscore
+    song.artistName = artistName
+    song.save()
+    return song
 
 def add_album(arts, albumID, albumName):
     alb = Album.objects.get_or_create(artistID=arts, albumID=albumID)[0]
