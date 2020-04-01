ce23de86079ab0cdc8c3483e2b9594f612b04199
Euan Wills <2389677W@student.gla.ac.uk>
2020-04-01 11:58:48 +0100

    had to push new database to pull new data

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from rango.models import UserProfile, Artist, Album, Song, Review
 
 admin.site.register(UserProfile) 
+
 #Apparently can't display the forgien key 'artistID'
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('albumID', 'albumName','slug')
