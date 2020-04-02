4d53e3bac749bba96d6579b797d75d126307a6ab
Euan Wills <2389677W@student.gla.ac.uk>
2020-04-02 23:46:24 +0100

    turning debug mode off

--- Applausable/settings.py
+++ Applausable/settings.py
@@ -31,7 +31,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = '^7j=2vk@varrl(c0w6*&96k$@wqf5jkkiwx82sa_b)q_s4h6*q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['EuanW.pythonanywhere.com']
 
