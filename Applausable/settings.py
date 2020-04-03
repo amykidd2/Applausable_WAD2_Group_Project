570df8381b8f2a3eec1a597ba4f6598d04f57c0b
Sam Walker <2379093w@student.gla.ac.uk>
2020-04-03 13:40:07 +0100

    added 127.0.0.1 to Allowed_Hosts so only debug needs to be changed when it is run locally

--- Applausable/settings.py
+++ Applausable/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '^7j=2vk@varrl(c0w6*&96k$@wqf5jkkiwx82sa_b)q_s4h6*q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['EuanW.pythonanywhere.com']
+ALLOWED_HOSTS = ['EuanW.pythonanywhere.com', '127.0.0.1']
 
 REGISTRATION_OPEN = True
 ACCOUNT_ACTIVATION_DAYS = 7
